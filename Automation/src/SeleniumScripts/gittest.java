76142317804995e094c092e73d3fedb29189a6b7
monik85 <78904907+monik85@users.noreply.github.com>
2021-07-16 11:31:14 +0530

    added in changes in file

--- Automation/src/SeleniumScripts/gittest.java
+++ Automation/src/SeleniumScripts/gittest.java
@@ -1,9 +1,22 @@
 package SeleniumScripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class gittest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+System.out.println("test");
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Praveen\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		
+    // System.setProperty("webdriver.chorme.driver","C:\\Users\\Praveen\\OneDrive\\Desktop\\chorme driver\\chromedriver_win32\\chromedriver.exe");
+		
+          WebDriver driver = new ChromeDriver();
+          driver.navigate().to("https://the-internet.herokuapp.com/javascript_alerts");
+          
 
 	}
 
